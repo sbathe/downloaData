c943214dd3f05577a2e81a4e74067d6f06c806e9
Saurabh Bathe <sbathe@hortonworks.com>
2018-10-22 14:24:26 +0530

    fix syntaxes

--- oop/download_data_files.py
+++ oop/download_data_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import requests, bs4
-import json
+import json, os
 import time
 
 class amfi:
@@ -20,12 +20,12 @@ class amfi:
 
     def get_amc_codes(self):
         url = 'https://www.amfiindia.com/nav-history-download'
-        r = self.get_data_from_url(url)
+        r = self.get_url_data(url)
         if r:
           soup = bs4.BeautifulSoup(r, 'html.parser')
           NavDownMFName =  soup.find_all("select",id="NavDownMFName")
           amc_codes = { e.string:e.attrs['value'] for e in NavDownMFName[0].findAll('option') if e.attrs['value'] != ''}
-       else:
+        else:
            amc_codes = None
         return amc_codes
 
@@ -37,14 +37,19 @@ class amfi:
         except:
             print("Unexpected error:{0}".format(sys.exc_info()[0]))
 
-    def get_amc_nav_data(self,amc,start_date=self.START_DATE,end_date=self.END_DATE):
+    def get_amc_nav_data(self,amc,start_date=None,end_date=None):
+        if start_date is None:
+            start_date = self.START_DATE
+        if end_date is None:
+            end_date = self.END_DATE
         url = self.NAV_URL_TEMPLATE + 'frmdt=' + start_date + '&todt=' + end_date + '&mf=' + amc
         data = self.get_url_data(url)
         return data
 
     def write_amc_files(self):
         amc_pairs = self.get_amc_codes()
-        for amc_name, amc_code in amc_pairs:
+        for amc_name, amc_code in amc_pairs.items():
             data = self.get_amc_nav_data(amc_code,start_date=self.START_DATE,end_date=self.END_DATE)
             filename = '_'.join(amc_name.split()) + '.csv'
             self.write_file(open(os.path.join('amfidata',filename),'a+'), data)
+            time.sleep(120)
