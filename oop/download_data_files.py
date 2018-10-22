e7700d448d501ce46126e371d06ab1dbefcb051e
Saurabh Bathe <sbathe@hortonworks.com>
2018-10-22 17:59:56 +0530

    added download function and some modifications to gitignore

--- oop/download_data_files.py
+++ oop/download_data_files.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import requests, bs4
 import json, os
-import time
+import time, datetime
 
 class amfi:
     def __init__(self):
         self.CODES_URL = 'https://www.amfiindia.com/nav-history-download'
         self.NAV_URL_TEMPLATE = 'http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?tp=1&'
         self.START_DATE = '01-Apr-2008'
-        self.END_DATE = time.strftime('%d-%b-%Y',time.localtime())
+        self.END_DATE = datetime.datetime.strftime(datetime.datetime.today(),'%d-%b-%Y')
 
     def get_url_data(self,url):
         try:
@@ -46,10 +46,34 @@ class amfi:
         data = self.get_url_data(url)
         return data
 
-    def write_amc_files(self):
+    def write_amc_files(self,start_date=None,end_date=None):
+        if start_date is None:
+            start_date = self.START_DATE
+        if end_date is None:
+            end_date = self.END_DATE
         amc_pairs = self.get_amc_codes()
+        lockdata = {}
+        today = datetime.datetime.strftime(datetime.datetime.strptime(end_date, '%d-%b-%Y'), '%s')
         for amc_name, amc_code in amc_pairs.items():
-            data = self.get_amc_nav_data(amc_code,start_date=self.START_DATE,end_date=self.END_DATE)
-            filename = '_'.join(amc_name.split()) + '.csv'
+            data = self.get_amc_nav_data(amc_code,start_date=start_date,end_date=end_date)
+            name = '_'.join(amc_name.split())
+            filename = '_'.join([name, today]) + '.csv'
             self.write_file(open(os.path.join('amfidata',filename),'a+'), data)
-            time.sleep(120)
+            lockdata[amc_name] = end_date
+            time.sleep(20)
+        lockdata['global'] = end_date
+        self.write_file(open('amfidata/lockfile.json','w+'), json.dumps(lockdata))
+
+    def init_or_update(self):
+        """ Returns startdate to get the data from, depending on when the
+        update was perfomed last"""
+        data_init = False
+        data_update = False
+        if os.path.isfile('amfidata/lockfile.json'):
+            d = json.load(open('amfidata/lockfile.json'))
+            return datetime.datetime.strftime(datetime.datetime.strptime(d['global'],'%d-%b-%Y') + datetime.timedelta(days=1), '%d-%b-%Y')
+        else:
+            return self.START_DATE
+    def download_data(self):
+        start_date = self.init_or_update()
+        self.write_amc_files(start_date=start_date)
