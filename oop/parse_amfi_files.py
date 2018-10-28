8adf0f3db64e0478d5e93a21c8400c5626d1d7cd
Saurabh Bathe <sbathe@hortonworks.com>
2018-10-28 08:01:39 +0530

    parse the data

--- oop/parse_amfi_files.py
+++ oop/parse_amfi_files.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 import requests, bs4
 import json, os
@@ -6,7 +5,7 @@ import time, datetime
 import re
 
 class AmfiParse:
-    def __init__(self):
+   def __init__(self):
         self.NODATA = re.compile('No data found on the basis of selected parameters for this report')
         self.HEADING = re.compile('Scheme Code;Scheme Name')
         self.OPENENDED = re.compile('Open Ended Scheme')
@@ -14,7 +13,7 @@ class AmfiParse:
 
    def nodata(self,raw_string):
        """ Checks if the data file has any useful info """
-       if re.search(self.NODATA,string):
+       if re.search(self.NODATA,raw_string):
            return True
        else:
            return False
@@ -23,59 +22,30 @@ class AmfiParse:
        """ This will remove all blank lines from the downloaded file. Returns
        a list of non-blank lines"""
        return [ line for line in raw_string.split('\n') if line.strip() ]
-
    def parse(self,processed_data):
        """ The goal is:
            - Create a scheme code wise json structure with 2 sections:
            - meta: a dictionary that has scheme name, fund AMC, category etc
            - data: list of (date, NAV) tuples
        """
-       categories = []
+       data = {}
        for line in processed_data:
            if re.search(self.HEADING,line):
-               headings = re.split(';')
+               headings = line.split(';')
            elif re.search(self.OPENENDED,line):
-              parts = l2.split('(')
+              c = []
+              parts = line.split('(')
               for p in parts:
                   c.append(p.split('-'))
                   categories = [item.strip(')').strip() for sublist in c for item in sublist]
-           elif re.search(self.AMC, line):
+           elif re.search(self.AMC,line):
               amc = line
+              data[amc] = {}
            elif len(line.split(';')) > 1:
                code,name,nav,rp,sp,date = line.split(';')
-               data[amc][code]["meta"] = { "name": name, "type": "open
-                                             ended", "categories": categories }
-               if "data" in data[amc][code].keys():
-                   data[amc][code]["data"].append([date,nav])
-               else:
-                   data[amc][code]["data"] = [date,nav]
-
-"""
-This still fails on assigining correct categories
-def parse(processed_data):
-     ...:        """ """The goal is:
-     ...:            - Create a scheme code wise json structure with 2 sections:
-     ...:            - meta: a dictionary that has scheme name, fund AMC, category etc
-     ...:            - data: list of (date, NAV) tuples
-     ...:        """ """
-     ...:        categories = []
-     ...:        for line in processed_data:
-     ...:            if re.search(HEADING,line):
-     ...:                headings = line.split(';')
-     ...:            elif re.search(OPENENDED,line):
-     ...:               parts = line.split('(')
-     ...:               for p in parts:
-     ...:                   c.append(p.split('-'))
-     ...:                   categories = [item.strip(')').strip() for sublist in c for item in sublist]
-     ...:            elif re.search(AMC,line):
-     ...:               amc = line
-     ...:               data[amc] = {}
-     ...:            elif len(line.split(';')) > 1:
-     ...:                code,name,nav,rp,sp,date = line.split(';')
-     ...:                if code not in data[amc].keys():
-     ...:                  data[amc][code] = {}
-     ...:                  data[amc][code]["data"] = []
-     ...:                  data[amc][code]["meta"] = { "name": name, "type": "open ended", "categories": categories }
-     ...:                  categories = []
-     ...:                data[amc][code]["data"].append([date,nav])
-"""
+               if code not in data[amc].keys():
+                 data[amc][code] = {}
+                 data[amc][code]["data"] = []
+                 data[amc][code]["meta"] = { "name": name, "type": "open ended", "categories": categories }
+               data[amc][code]["data"].append([date,nav])
+       return data
