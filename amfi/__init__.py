47a1e97b031fa5ebd18e7cad809a19228046329a
Saurabh Bathe <sbathe@hortonworks.com>
2018-10-28 14:42:42 +0530

    package it

---
+++ amfi/__init__.py
@@ -0,0 +1,2 @@
+import amfi.download_data_files
+import amfi.parse_amfi_files
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='amfi',
+    version='0.1.0',
+    url='https://github.com/sbathe/downloadData',
+    license='GPLv3',
+    author='Saurabh Bathe',
+    author_email='sbathe@gmail.com',
+    packages=setuptools.find_packages(),
+    description='put something good here',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+)
