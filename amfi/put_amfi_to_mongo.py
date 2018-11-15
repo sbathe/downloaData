83073eba061eba97061f0112338e379f2a06d0a8
Saurabh Bathe <sbathe@gmail.com>
2018-11-15 14:57:00 +0530

    works

--- amfi/put_amfi_to_mongo.py
+++ amfi/put_amfi_to_mongo.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 from amfi import AmfiDownload
 from amfi import AmfiParse
-import pymongo
+import pymongo, datetime
 
 class AmfiMongo:
     def __init__(self):
         # pull config data here
         client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
         self.DB = client.amfi
+        if not 'meta' in self.DB.list_collection_names():
+            self.mcoll = self.DB.create_collection('meta')
+            self.mcoll.create_index([('scheme_code',pymongo.ASCENDING)],unique=True)
+            self.mcoll.create_index([('name', pymongo.TEXT), ('amc',pymongo.TEXT)])
 
     def write_jsons_to_docments(self, in_dir):
        amfidownloadobj = AmfiDownload()
        amfiparseobj = AmfiParse()
        amc_pairs = amfidownloadobj.get_amc_codes()
+       mcoll = self.DB.get_collection('meta')
        for amc_name, amc_code in amc_pairs.items():
          data = amfiparseobj.get_json_from_amc_csvs(amc_name,in_dir)
          if not data:
              continue
          for scheme_code in data[amc_name].keys():
-             scheme_meta = data[amc_name][scheme_code]['meta']
+             #scheme_meta = data[amc_name][scheme_code]['meta']
+             try:
+                 mcoll.insert_one(data[amc_name][scheme_code]['meta'])
+             except pymongo.errors.DuplicateKeyError as e:
+                 pass
+                 #print('{0} : {1}'.format(scheme_code,e))
              scheme_data = data[amc_name][scheme_code]['data']
              collname = 'c'+scheme_code
              if not collname in self.DB.list_collection_names():
                  coll = self.DB.create_collection(collname)
                  for ufield in ['date']:
                      coll.create_index([(ufield,pymongo.ASCENDING)],unique=True)
-                 for field in ['scheme_code', 'name']:
-                     coll.create_index(field)
-                 coll.insert_one(scheme_meta)
+                 #for field in ['scheme_code', 'name']:
+                 #    coll.create_index(field)
+                 #coll.insert_one(scheme_meta)
              else:
                  coll = self.DB.get_collection(collname)
              for element in data[amc_name][scheme_code]['data']:
@@ -36,7 +46,8 @@ class AmfiMongo:
                  try:
                      coll.insert_one(element)
                  except pymongo.errors.DuplicateKeyError as e:
-                     print('{0} : {1}'.format(element,e))
+                     pass
+                     #print('{0} : {1}'.format(element,e))
        """
        for amc_name, amc_code in amc_pairs.items():
            data = amfiparseobj.get_json_from_amc_csvs(amc_name,in_dir)
