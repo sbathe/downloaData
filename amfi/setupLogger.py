5914e9406c683badaf02b07b9b7b0f2cc15ef5a0
Saurabh Bathe <sbathe@gmail.com>
2019-08-28 23:27:38 +0530

    remove setuplogger.py

--- amfi/setupLogger.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.DEBUG,
-                     handlers=[logging.StreamHandler(sys.stdout)])
-logger = logging.getLogger(__name__)
